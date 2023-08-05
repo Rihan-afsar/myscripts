a1125fece82404d5521faa8ce8f71fb3a29cf273
rihan-afsar <root@DESKTOP-PLTGLES>
2023-08-06 00:40:19 +0530

    1st commit in linux

---
+++ 2nd_comments.sh
@@ -0,0 +1,11 @@
+echo "checking comments"
+
+#This is the singal commets.
+
+<<end
+
+this
+is multi
+line
+cmments
+end
---
+++ 3rd_var.sh
@@ -0,0 +1,8 @@
+#script to show how to use variable.
+
+a=10
+name="rihan"
+age=28
+
+echo "My name is $name"
+echo "age is $age"
